93c64d993bccc5cfc5a3d92de10dada4f36bfe01
Irene Monlleó <irene.monlleo@gmail.com>
2023-07-20 22:15:36 +0200

    Created user avatar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AvatarComponent } from './avatar/avatar.component';
+import { UserAvatarComponent } from './user-avatar/user-avatar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AvatarComponent
+    AvatarComponent,
+    UserAvatarComponent
   ],
   imports: [
     BrowserModule,
