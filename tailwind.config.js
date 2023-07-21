5a47f3a4c3afe195bfd6facd9e250e7bb2effdc5
Irene Monlleó <irene.monlleo@gmail.com>
2023-07-21 21:43:20 +0200

    Customize colors in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    colors: {
+      'blue': '#0081f0',
+      'dark-blue': '#31394c',
+      'light-grey': '#fafafa',
+      'grey': '#d7d7d7',
+      'text-grey':'#777777',
+      'grey-blue': '#697082',
+      'purple': '#9d57b7',
+    },
     extend: {},
   },
   plugins: [],
